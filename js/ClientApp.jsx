f767b26b7b53319660a79b1b09936dc4bf987f3c
koyexes <koyagabriel@gmail.com>
2017-11-12 19:32:38 +0100

    integrate flow

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,9 +1,10 @@
+// @flow
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
 const renderApp = () => {
-  render(<App />, document.getElementById('app'))
+  render(<App />, document.getElementById('app'));
 };
 renderApp();
 
